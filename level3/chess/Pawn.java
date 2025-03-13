dd79456593d1632962c8a31957bdeb93ae3ba566
Abin-tek <abinathmouniane@gmail.com>
2025-03-13 16:05:16 +0530

    pawn started

--- level3/chess/Pawn.java
+++ level3/chess/Pawn.java
@@ -1,5 +1,28 @@
 public class Pawn extends Piece {
+    private  boolean first_move;
+
     public Pawn(boolean white) {
         super(white);
+        first_move = true;
+    }
+
+    @Override
+    public boolean canMove() {
+        int step = 1, fl = 1, fr = 1;
+
+        if (first_move) {
+            step = 2;
+            first_move = false;
+        }
+
+        return super.canMove();
+    }
+
+    @Override
+    public boolean isPathClear() {
+        if (first_move){
+            return false;
+        }
+        return true;
     }
 }
