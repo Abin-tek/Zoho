62fb764fa8b09d076b4cfeb063eb18548dcb41b2
Abin-tek <abinathmouniane@gmail.com>
2025-03-13 12:04:14 +0530

    delegates done

--- level3/chess/Main.java
+++ level3/chess/Main.java
@@ -1,20 +1,9 @@
 
 public class Main {
-	class MyThread extends Thread{
-		@Override
-		public void run() {
-			EventDelegationExample d1 = new EventDelegationExample();
-			EventDelegationExample d2 = new EventDelegationExample();
-			d1.start();
-			d2.start();
-		}
-	}
+
 
 	public static void main(String[] args) {
-		MyThread t1 = new Main().new MyThread();
-		MyThread t2 = new Main().new MyThread();
-		t1.start();
-		t2.start();
+
 	}
 
 }
